1454819cb2541c7149c30c80a69ff0ebdf90d36f
Locas <103526154+llazypig@users.noreply.github.com>
2024-08-21 13:48:37 +0800

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -1,6 +1,7 @@
 import os
 import time
 import oss2
+from tqdm import tqdm
 
 # 本地文件夹路径目录
 basedir = '/home/monster/Desktop/share/DCIM/hc'
@@ -12,7 +13,7 @@ dirList = [basedir]
 bucket = 'stt-cctv'  # 设置为你的存储桶名称
 ossDir = 'b-cctv/'  # 替换为你实际的目录
 # 将accessKeyId 和 accessKeySecret替换自己的
-ossAuth = oss2.Auth('accessKeyId', 'accessKeySecret')
+ossAuth = oss2.Auth('yourAccessKeyId', 'yourAccessKeySecret')
 # 使用正确的端点
 ossBucket = oss2.Bucket(ossAuth, 'http://oss-cn-hongkong.aliyuncs.com', bucket)
 
@@ -32,9 +33,22 @@ def uploadFile(file):
     # remoteName为oss目录名称, file为本地上传目录名称
     remoteName = ossDir + file.replace(basedir, '').replace('\\', '/')
     print('uploading..', file, 'remoteName', remoteName)
-    result = ossBucket.put_object_from_file(remoteName, file)
-    # 文件上传成功http状态输出200
-    print('http status: {0}'.format(result.status))
+    
+    # 获取文件大小
+    file_size = os.path.getsize(file)
+    
+    # 定义进度回调函数
+    def progress_callback(consumed_bytes, total_bytes):
+        progress_bar.update(consumed_bytes - progress_bar.n)
+    
+    # 创建进度条
+    with tqdm(total=file_size, unit='B', unit_scale=True, desc=file) as progress_bar:
+        try:
+            result = ossBucket.put_object_from_file(remoteName, file, progress_callback=progress_callback)
+            # 文件上传成功http状态输出200
+            print('http status: {0}'.format(result.status))
+        except oss2.exceptions.OssError as e:
+            print(f'Error uploading {file}: {e}')
 
 def listFile(dir):
     fs = os.listdir(dir)
